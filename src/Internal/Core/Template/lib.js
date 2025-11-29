3bbe226b3e417db3a910218496e9da51725103a5
R3DG0BL1N <58450310+R3DG0BL1N@users.noreply.github.com>
2025-11-29 13:12:22 +0100

    add(lib.js): added backup not mimified js for template.

---
+++ src/Internal/Core/Template/lib.js
@@ -0,0 +1,26 @@
+function request(api, method, data = {}){
+    return new Promise(function(resolve, reject){
+        $.ajax({
+            url: api,
+            method: method,
+            data: data,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': window.CSRF_TOKEN ?? ''
+            },
+            success: function(response){
+                resolve(response);
+            },
+            error: function(xhr, status, error){
+                reject(error);
+            }
+        });
+    });
+}
+
+for(const req of []) {
+    request(req["api"], "GET").then(data => {
+        const result = data.data;
+        $(req["dom"]).html(result.toString());
+    })
+}
